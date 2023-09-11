a245213fc0d16c35f6c39336a53c57bdf790b4e0
Leb-han <nkabindelebohang3@gmail.com>
2023-09-10 23:35:36 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - printing single digit numbers based on 10 starting from 0
+ * Return:Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+	putchar('\n');
+	return (0);
+}
