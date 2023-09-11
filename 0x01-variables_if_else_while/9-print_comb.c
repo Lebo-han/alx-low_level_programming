f9596f5edccd16f15154cc81f16d1cb34ed45afc
Leb-han <nkabindelebohang3@gmail.com>
2023-09-11 01:16:55 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -  possible combinations of single-digit numbers
+ * Return:Always 0
+ */
+
+int main(void)
+{
+	int t;
+
+	for (t = 0; t < 10 ; t++)
+	{
+		putchar(t + '0');
+		if (t < 9)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
