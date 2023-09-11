32f5c2535ffda02767f4a973051f963ce23ccea6
Leb-han <nkabindelebohang3@gmail.com>
2023-09-10 23:50:09 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Single digits numbers
+ * Return:Always 0
+ */
+
+int main(void)
+{
+	int u;
+
+	u = 0;
+	while (u < 10) {
+		putchar(u + '0');
+		u++;
+	}
+	putchar('\n');
+	return (0);
+}
